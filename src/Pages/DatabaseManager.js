8a18817e649e362fe594114198b90741546fe333
Caio Vinícius Menezes Gomes <c214192@dac.unicamp.br>
2024-11-03 17:51:04 -0300

    Deletar, modal confirmando e bug fixes

--- src/Pages/DatabaseManager.js
+++ src/Pages/DatabaseManager.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { database } from "../firebase";
-import { ref, onValue, } from "firebase/database";
+import { ref, onValue, set } from "firebase/database";
 
 function DatabaseManager() {
   const [databaseName, setDatabaseName] = useState("bee_data");
   const [dataRef, setDataRef] = useState(ref(database, "bee_data"));
   const [placasDisponiveis, setPlacasDisponiveis] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [alert, setAlert] = useState({ visible: false, message: "", type: "" });
+
   const topLevelEntriesRef = ref(database, "/");
 
   useEffect(() => {
@@ -13,15 +16,44 @@ function DatabaseManager() {
       if (snapshot.exists()) {
         const topLevelEntries = Object.keys(snapshot.val());
         setPlacasDisponiveis(topLevelEntries);
+        setDatabaseName(topLevelEntries[0]);
       }
     });
   }, []);
+
+  useEffect(() => {
+    setDataRef(ref(database, databaseName));
+  }, [placasDisponiveis]);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  const handleDeleteAll = () => {
+    set(dataRef, "")
+      .then(() => {
+        setAlert({
+          visible: true,
+          message: "Dados deletados com sucesso!",
+          type: "success",
+        });
+        setIsModalOpen(false);
+      })
+      .catch((error) => {
+        setAlert({
+          visible: true,
+          message: "Erro ao tentar deletar os dados!",
+          type: "error",
+        });
+      });
+  };
+
+  const closeAlert = () => setAlert({ ...alert, visible: false });
 
   return (
     <div className="App">
       <main className="Main-Body">
         <div id="btns">
-        <h2>Selecione o nome da placa que deseja gerenciar: </h2>
+          <h2>Selecione o nome da placa que deseja gerenciar:</h2>
           <select
             className="styled-textfield"
             value={databaseName}
@@ -39,10 +71,42 @@ function DatabaseManager() {
               </option>
             ))}
           </select>
+          <button onClick={openModal}>Deletar Dados</button>
         </div>
         <div id="btns">
-          
+         O que colocar aqui?
         </div>
+
+        {isModalOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <h3>Atenção!!!</h3>
+              <p>
+                Você tem certeza de que gostaria de deletar todos os dados da
+                placa {databaseName} ?
+              </p>
+              <button onClick={handleDeleteAll}>Sim, quero deletar</button>
+              <button onClick={closeModal}>Cancelar</button>
+            </div>
+          </div>
+        )}
+
+        {alert.visible && (
+          <div
+            className={`alert ${
+              alert.type === "success" ? "alert-success" : "alert-error"
+            }`}
+            style={{ position: "absolute", top: '27vh', right: 20 }}
+          >
+            {alert.message}
+            <button
+              onClick={closeAlert}
+              style={{ marginLeft: "10px" }}
+            >
+              x
+            </button>
+          </div>
+        )}
       </main>
     </div>
   );
